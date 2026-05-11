24049bd52ee2d58bdbf04a2948601975ab779454
d.aerle <dvanaerle@outlook.com>
2026-05-11 16:54:01 +0200

    Added: Bamboe Vlonders

--- app/routes.ts
+++ app/routes.ts
@@ -2,7 +2,7 @@ import { type RouteConfig, index, route } from "@react-router/dev/routes";
 
 export default [
   index("routes/home.tsx"),
-  route("configurator", "routes/Configurator.tsx"),
+  route("configurator", "routes/configurator.tsx"),
   route("productinfopage", "routes/productinfopage.tsx"),
   route("tuinmaximaal-styleguide", "routes/tuinmaximaal-styleguide.tsx"),
 ] satisfies RouteConfig;
